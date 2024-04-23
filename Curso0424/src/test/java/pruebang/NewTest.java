a2e6ecc74618b80a2127f8860d1f7b8f9bd48236
Borrego <Borrego@DESKTOP-LK9S417>
2024-04-23 14:08:13 -0600

    Primer Commit

---
+++ Curso0424/src/test/java/pruebang/NewTest.java
@@ -0,0 +1,15 @@
+package pruebang;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void test01() {
+	  
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+		
+  }
+}
